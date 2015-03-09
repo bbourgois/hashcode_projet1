eac8c2461fddd61b6b425b986b21b3ccc836e150
Blandine <blandine.bourgois@gmail.com>
2015-03-10 00:17:10 +0100

    second commit

---
+++ src/main/java/fr/bernie/team/hashcode/projet1/Test.java
@@ -0,0 +1,16 @@
+package fr.bernie.team.hashcode.projet1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by bbo on 09/03/15.
+ */
+public class Test {
+
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        log.info("test Test");
+    }
+}
